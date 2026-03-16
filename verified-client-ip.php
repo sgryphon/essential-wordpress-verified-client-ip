67adea24eafd530276a207a87fff0cf1aea07eb5
Sly Gryphon <sgryphon@computer.org>
2026-03-16 00:12:11 +0000

    Fix domain path issue

--- verified-client-ip.php
+++ verified-client-ip.php
@@ -12,7 +12,6 @@
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       verified-client-ip
- * Domain Path:       /languages
  *
  * @package VerifiedClientIp
  */
